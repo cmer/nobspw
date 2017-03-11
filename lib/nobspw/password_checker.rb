d753f61bf207c248abdcde460663cb47ca1954f2
Carl Mercier <carl@carlmercier.com>
2017-03-11 13:18:10 -0500

    Do not require configuration

--- lib/nobspw/password_checker.rb
+++ lib/nobspw/password_checker.rb
@@ -12,6 +12,7 @@ module NOBSPW
     def initialize(name: nil, username: nil, email: nil, password:)
       @name, @username, @email, @password = \
         name&.strip, username&.strip, email&.strip, password&.strip
+      NOBSPW.configuration ||= NOBSPW::Configuration.new
     end
 
     def strong?
