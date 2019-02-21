edebe1c9e7a3cd14a48b994856db01ea4f95b091
Carl Mercier <foss@carlmercier.com>
2019-02-21 14:16:29 -0500

    Added grep benchmark

---
+++ misc/grep_benchmark.rb
@@ -0,0 +1,21 @@
+#! /usr/bin/env ruby
+
+require 'benchmark'
+
+password = 'swordfish'
+dictionary_path = File.join(File.dirname(__FILE__), '..', 'lib/db/dictionary.txt')
+
+def shell_grep(password, dictionary)
+	"/usr/bin/grep '^#{password}$' #{dictionary}"
+  $?.exitstatus == 0
+end
+
+Benchmark.bm do |benchmark|
+  benchmark.report("Ruby") do
+    25.times { File.open(dictionary_path).grep(/^#{password}$/) }
+  end
+
+  benchmark.report("Shell") do
+    25.times { shell_grep(password, dictionary_path) }
+  end
+end
